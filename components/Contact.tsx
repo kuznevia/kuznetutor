da9781677dd327af6faca4fe9811535a147447d2
Viacheslav <vyac.v.kuznetsov@rt.ru>
2023-03-17 18:56:58 +0300

    adding custom Input and TextArea components

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -57,7 +57,10 @@ const Contact = () => (
       <div className="flex justify-center py-12">
         <Link href="/">
           <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 hover:shadow-gray-700 ease-in duration-300">
-            <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30} />
+            <HiOutlineChevronDoubleUp
+              className="text-[var(--color-secondary)]"
+              size={30}
+            />
           </div>
         </Link>
       </div>
