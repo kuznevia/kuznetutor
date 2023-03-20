dea834e8b4db559ba3b02752705d5089e99624a6
kuznevia <56788347+kuznevia@users.noreply.github.com>
2023-03-20 22:59:40 +0700

    changing design colors (#16)
    * changing design colors and adding css variables
    
    * adding custom Input and TextArea components
    
    * changing background and outline for input elements
    
    * changes
    
    1. Changing design theme to black and mint
    2. Adding real images
    3. Adding ui component for Icons
    
    ---------
    
    Co-authored-by: Viacheslav <vyac.v.kuznetsov@rt.ru>

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import AboutImg from 'public/assets/about.jpg';
+import Nadya from 'public/assets/nadya-2.jpeg';
 import React from 'react';
 
 const About = () => {
@@ -8,9 +8,8 @@ const About = () => {
     <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
-          <p className="uppercase text-xl tracking-widest text-[#5651e5]">About</p>
-          <h2 className="py-4">Who I Am</h2>
-          <p className="py-2 text-gray-600">
+          <h2 className="py-4 text-[var(--color-secondary)]">Обо мне</h2>
+          <p className="py-2">
             I specialize in building mobile responsive front-end UI applications that
             connect with API’s and other backend technologies. I’m passionate about
             learning new technologies and understand there is more than one way to
@@ -19,7 +18,7 @@ const About = () => {
             can pick up new tech stacks as needed. I believe that being a great developer
             is not using one specific language, but choosing the best tool for the job.
           </p>
-          <p className="py-2 text-gray-600">
+          <p className="py-2">
             I started web developement in 2013 managing multiple e-commerce websites on
             CMS platforms such as WordPress, BigCommerce, and Shopify. I have experience
             working directly with clients and taking mock wireframes all the way to
@@ -27,13 +26,13 @@ const About = () => {
             where I teach web developement and various front-end technologies.
           </p>
           <Link href="/#projects">
-            <p className="py-2 text-gray-600 underline cursor-pointer">
+            <p className="py-2 underline cursor-pointer">
               Check out some of my latest projects.
             </p>
           </Link>
         </div>
-        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <Image src={AboutImg} className="rounded-xl" alt="/" />
+        <div className="w-full bg-[var(--bg-secondary)] h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <Image src={Nadya} className="rounded-xl" alt="/" />
         </div>
       </div>
     </div>
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,3 +1,6 @@
+import { Icon } from 'components/ui/Icon';
+import Image from 'next/image';
+import Nadya from 'public/assets/nadya.jpeg';
 import React from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
@@ -6,27 +9,24 @@ import { SlSocialVkontakte } from 'react-icons/sl';
 
 const Main = () => {
   return (
-    <div id="home" className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <div>
-          <h1 className="py-4 text-[#5651e5]">Надежда Кузнецова</h1>
-          <h1 className="py-2 text-gray-700">Репетитор по истории и обществознанию</h1>
-          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
-            Какой то длинный текст который что-то рассказывает про тебя и возможно твой
-            опыт и еще что-нибудь еще.
-          </p>
-          <div className="flex items-center justify-between max-w-[330px] m-auto p-4">
-            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 hover:shadow-gray-700 ease-in duration-300">
-              <SlSocialVkontakte />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 hover:shadow-gray-700 ease-in duration-300">
-              <FiPhoneCall />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 hover:shadow-gray-700 ease-in duration-300">
-              <AiOutlineMail />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 hover:shadow-gray-700 ease-in duration-300">
-              <BsFillPersonLinesFill />
+    <div id="home" className="w-full md:h-screen p-2 text-center flex items-center pt-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="hidden w-full bg-[var(--bg-secondary)] h-auto m-auto shadow-xl shadow-gray-400 rounded-xl md:flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <Image src={Nadya} className="rounded-xl" alt="/" />
+        </div>
+        <div className="col-start-2 col-end-4 max-w-[1240px] p-2 flex justify-center items-center">
+          <div>
+            <h1 className="py-4 text-[var(--color-secondary)]">Надежда Кузнецова</h1>
+            <h1 className="py-2">Репетитор по истории и обществознанию</h1>
+            <p className="py-4 max-w-[70%] m-auto">
+              Какой то длинный текст который что-то рассказывает про тебя и возможно твой
+              опыт и еще что-нибудь еще.
+            </p>
+            <div className="flex items-center justify-between max-w-[330px] m-auto p-4">
+              <Icon Icon={SlSocialVkontakte} />
+              <Icon Icon={FiPhoneCall} />
+              <Icon Icon={AiOutlineMail} />
+              <Icon Icon={BsFillPersonLinesFill} />
             </div>
           </div>
         </div>
