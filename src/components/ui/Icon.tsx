e444f4031e1e40b1632c16ed537d7251db804ab5
Viacheslav <vyac.v.kuznetsov@rt.ru>
2023-04-14 15:34:32 +0300

    adds links to icons + adds links storage

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -1,20 +1,34 @@
+import Link from 'next/link';
 import React from 'react';
 import { IconType } from 'react-icons';
 
-export const Icon = ({ Icon, nav, ...rest }: { Icon: IconType; nav?: boolean }) => {
+export const Icon = ({
+  Icon,
+  nav,
+  link,
+  ...rest
+}: {
+  Icon: IconType;
+  link: string;
+  nav?: boolean;
+}) => {
   if (nav) {
     return (
-      <div className="bg-[var(--bg-secondary)] text-white rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 hover:shadow-gray-700 ease-in duration-300">
-        <Icon />
-      </div>
+      <Link href={link}>
+        <div className="bg-[var(--bg-secondary)] text-white rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 hover:shadow-gray-700 ease-in duration-300">
+          <Icon />
+        </div>
+      </Link>
     );
   }
   return (
-    <div
-      className="bg-[var(--bg-secondary)] text-white rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 hover:shadow-gray-700 ease-in duration-300"
-      {...rest}
-    >
-      <Icon />
-    </div>
+    <Link href={link}>
+      <div
+        className="bg-[var(--bg-secondary)] text-white rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 hover:shadow-gray-700 ease-in duration-300"
+        {...rest}
+      >
+        <Icon />
+      </div>
+    </Link>
   );
 };
